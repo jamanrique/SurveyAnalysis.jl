2b084df18e0e5088a24ceeb0c84646075450f7d6
jamanrique <ja.manrique@pm.me>
2020-08-04 05:07:50 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.7

---
+++ src/SurveyAnalysis.jl
@@ -0,0 +1,5 @@
+module SurveyAnalysis
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SurveyAnalysis
+using Test
+
+@testset "SurveyAnalysis.jl" begin
+    # Write your tests here.
+end
