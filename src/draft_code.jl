e77348803d145094ba5fe1cb5bf0a7f223e9a35f
jamanrique <ja.manrique@pm.me>
2020-08-10 19:03:21 -0500

    Random sort SRSWOR

--- src/draft_code.jl
+++ src/draft_code.jl
@@ -9,11 +9,22 @@ function svySampling(DataFrame::AbstractDataFrame,SampleSize::Int)
 	for i in 1:nrow(DataFrame)
 		Randᵤ= rand(μ,1)
 		if in(Randᵤ,A)
-
-
 	end
 end
 
-function surveydesign(DataFrame,)
+function svySRS(DataFrame::AbstractDataFrame, SampleSize::Int)
+	## Procedure: Random sort SRSWOR ##
+	## Based in Algorithm 4.5 from Yves Tillé book ##
+	## For each of the DataFrame rows, we generate a random value from a [0,1] Uniform distribution. After this, we sort the DataFrame and select the desired sample size (based in SampleSize value) ##
+	μ = Uniform()
+	Rand = rand(μ,nrow(DataFrame))
+	DataFrame₂= hcat(DataFrame,Rand)
+	sort!(DataFrame₂,:x1,rev=true)
+	return DataFrame₂[1:SampleSize,:]
+
+	## How to put logs here?
 end
+	
+svySRS(iris,20)
+
 
