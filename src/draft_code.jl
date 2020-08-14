9d984679c974fedd9f0274e9a7a9d31bef33ddd9
jamanrique <ja.manrique@pm.me>
2020-08-13 22:42:37 -0500

    SRS Reservoir

--- src/draft_code.jl
+++ src/draft_code.jl
@@ -21,10 +21,28 @@ function svySRS(DataFrame::AbstractDataFrame, SampleSize::Int)
 	DataFrame₂= hcat(DataFrame,Rand)
 	sort!(DataFrame₂,:x1,rev=true)
 	return DataFrame₂[1:SampleSize,:]
-
 	## How to put logs here?
 end
-	
-svySRS(iris,20)
 
+function svyReservoirSRS(DataFrame::AbstractDataFrame,SampleSize::Int)
+	## Procedure: Reservoir method for SRSWOR
+	## Select the first n units, then for the remaining values
+	## with probability n/k (Bernoulli) select this unit and replace it in the selected sample. This replacement
+	## is at random.
+	DataFrame₂ = first(DataFrame,SampleSize)
+	β = Bernoulli(SampleSize/nrow(DataFrame))
+	μ = DiscreteUniform(1,SampleSize)
+	DataFrameₖ = last(DataFrame,nrow(DataFrame)-SampleSize)
+	for row in eachrow(DataFrameₖ)
+		Randᵦ = rand(β,1)
+		if Randᵦ==[1]
+			Randᵤ= rand(μ,1)
+			DataFrame₂[Randᵤ,:] = DataFrame(row) ## current problem: can't replace row! broadcasting issues.
+		end
+	end
+	return DataFrame₂
+end
+
+svySRS(iris,20)
+svyReservoirSRS(iris,20)
 
